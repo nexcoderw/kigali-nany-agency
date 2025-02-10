833b8c60300b2412933bf415db05f0c16836249b
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-10 18:49:07 +0200

    Add routes for main pages

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,9 @@ app_name = 'base'
 
 urlpatterns = [
     path('', home, name="home"),
+    path('about', about, name="about"),
+    path('services', services, name="services"),
+    path('jobs', getJobs, name="getJobs"),
+    path('job/slug', showJob, name="showJob"),
+    path('contact', contact, name="contact"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
