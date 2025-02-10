589829a201c1cd0f2c8c682aaac5b3f154370344
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-10 18:50:30 +0200

    Add view function for pages

--- base/views.py
+++ base/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def getJobs(request):
+    return render(request, 'pages/jobs/index.html')
+
+def showJob(request):
+    return render(request, 'pages/jobs/show.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
