abd15c5b1e5dab056b23f176a8dc19e0bcd1d35f
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-14 17:41:56 +0200

    Add dashboard route and view function

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,6 @@ app_name = 'base'
 
 urlpatterns = [
     path('', home, name="home"),
+
+    path('dashboard/', dashboard, name="dashboard"),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- base/views.py
+++ base/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/index.html')
+
+def dashboard(request):
+    return render(request, 'pages/user/dashboard.html')
