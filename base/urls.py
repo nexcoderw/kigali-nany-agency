718b92007ae7591e6cec7c4e55a87be2985c8d60
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-14 15:58:52 +0200

    Add home route and view function

---
+++ base/urls.py
@@ -0,0 +1,10 @@
+from base.views import *
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'base'
+
+urlpatterns = [
+    path('/', home, name="home"),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
