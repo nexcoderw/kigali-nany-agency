c78eb9a861d2d5c02bfe38319254a1edad14dd9b
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-14 15:38:35 +0200

    Add login view function

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'account'
+app_name = 'auth'
 
 urlpatterns = [
     path('login', userLogin, name="login"),
--- account/views.py
+++ account/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from account.forms import *
+from account.models import *
+from django.urls import reverse
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth import authenticate, login as auth_login, logout, update_session_auth_hash
 
-# Create your views here.
+def userLogin(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.info(request, _("You have been logged out because you accessed the login page while already logged in."))
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data.get('user')
+            auth_login(request, user)
+            messages.success(request, _("Welcome back! You have successfully logged in."))
+            return redirect(reverse('base:home'))
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, error)
+            messages.error(request, _("Please address the errors below and try again."))
+    else:
+        form = LoginForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'pages/auth/login.html', context)
