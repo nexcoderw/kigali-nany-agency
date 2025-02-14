3ab498562d2aa0a06889728a9f95f7c04d0fe80f
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-14 15:23:39 +0200

    New project setup

--- base/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, NannyProfile, ClientProfile, Booking, Review
-
-admin.site.register(UserProfile)
-admin.site.register(NannyProfile)
-admin.site.register(ClientProfile)
-admin.site.register(Booking)
-admin.site.register(Review)
