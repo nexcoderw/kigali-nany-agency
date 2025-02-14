161f83803e10c121d30366007e598e9aa3e473e8
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-14 16:02:24 +0200

    New changes

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 app_name = 'base'
 
 urlpatterns = [
-    path('/', home, name="home"),
+    path('', home, name="home"),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
