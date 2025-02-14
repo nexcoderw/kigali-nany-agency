e40274d0d7e36b1290dc84409b0b099038441497
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-14 15:31:52 +0200

    Add routes for login, logout and register

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+from account.views import *
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'account'
+
+urlpatterns = [
+    path('login', userLogin, name="login"),
+    path('logout/', userLogout, name='logout'),
+    path('register', userRegister, name="register"),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
