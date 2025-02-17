08d72e56c32f0ec4cab238758fa18cf5689345b3
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-17 14:29:29 +0200

    Registered JobApplication model in admin with custom list view and filters

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
-from base.models import JobPosting, JobCategory, JobStatus
+from base.models import JobPosting, JobCategory, JobStatus, JobApplication
 from django.contrib.auth import get_user_model
 
 @admin.register(JobPosting)
@@ -9,8 +9,6 @@ class JobPostingAdmin(admin.ModelAdmin):
     """
     Custom JobPostingAdmin for managing job postings in Django admin.
     """
-
-    # Display these fields in the admin list view
     list_display = (
         'title',
         'client',
@@ -25,19 +23,11 @@ class JobPostingAdmin(admin.ModelAdmin):
         'slug',
     )
 
-    # Fields to search by
     search_fields = ('title', 'client__email', 'location')
-
-    # Filters for the sidebar
     list_filter = ('category', 'status', 'created_at', 'updated_at')
-
-    # Ordering of the list view
     ordering = ('-created_at',)
-
-    # Read-only fields in the admin detail view
     readonly_fields = ('slug', 'created_at', 'updated_at')
 
-    # Fieldsets to organize fields in the admin detail view
     fieldsets = (
         (None, {
             'fields': ('title', 'description', 'category', 'status', 'salary', 'location', 'start_date', 'end_date', 'client')
@@ -50,7 +40,6 @@ class JobPostingAdmin(admin.ModelAdmin):
         }),
     )
 
-    # Fieldsets for the add JobPosting form
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
@@ -58,23 +47,19 @@ class JobPostingAdmin(admin.ModelAdmin):
         }),
     )
 
-    # Display category in the list view as human-readable
     def category_display(self, obj):
         return obj.get_category_display()
     category_display.short_description = 'Category'
 
-    # Display status in the list view as human-readable
     def status_display(self, obj):
         return obj.get_status_display()
     status_display.short_description = 'Status'
 
-    # Override save method to ensure the slug is always unique and correctly generated
     def save_model(self, request, obj, form, change):
         if not obj.slug:
             obj.slug = obj.generate_unique_slug()
         super().save_model(request, obj, form, change)
 
-    # Add custom actions if necessary (optional)
     actions = ['make_open', 'make_closed', 'make_pending', 'make_in_progress']
 
     @admin.action(description='Mark selected jobs as open')
@@ -93,8 +78,51 @@ class JobPostingAdmin(admin.ModelAdmin):
     def make_in_progress(self, request, queryset):
         queryset.update(status=JobStatus.IN_PROGRESS)
 
-    # Modify the client field to only show users with 'Client' role
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
         form.base_fields['client'].queryset = get_user_model().objects.filter(role='Client')
         return form
+
+
+@admin.register(JobApplication)
+class JobApplicationAdmin(admin.ModelAdmin):
+    """
+    Custom JobApplicationAdmin for managing job applications in Django admin.
+    """
+    list_display = (
+        'nanny_name',
+        'job_title',
+        'status_display',
+        'applied_at',
+        'updated_at',
+    )
+
+    search_fields = ('nanny__email', 'job__title', 'status')
+    list_filter = ('status', 'applied_at', 'updated_at')
+    ordering = ('-applied_at',)
+    readonly_fields = ('applied_at', 'updated_at')
+
+    fieldsets = (
+        (None, {
+            'fields': ('nanny', 'job', 'experience', 'availability', 'cover_letter', 'status')
+        }),
+        (_('Dates'), {
+            'fields': ('applied_at', 'updated_at')
+        }),
+    )
+
+    def nanny_name(self, obj):
+        return obj.nanny.name
+    nanny_name.short_description = 'Nanny Name'
+
+    def job_title(self, obj):
+        return obj.job.title
+    job_title.short_description = 'Job Title'
+
+    def status_display(self, obj):
+        return obj.get_status_display()
+    status_display.short_description = 'Status'
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
