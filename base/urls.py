5a621b7390ef51891a05df71a72813379be4ff5d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-18 14:17:26 +0200

    Add template to retrieve all hire applications for nanny

--- base/urls.py
+++ base/urls.py
@@ -30,8 +30,8 @@ urlpatterns = [
     path('client/hire-applications/<id>/', getHireApplicationDetails, name="getHireApplicationDetails"),
 
     # Nanny
-    path('nanny/job-applications/', getJobApplications, name="getJobApplications"),
-    path('nanny/job-application/<id>/', getJobApplicationDetails, name="getJobApplicationDetails"),
+    path('job-applications/', getJobApplications, name="getJobApplications"),
+    path('job-application/<id>/', getJobApplicationDetails, name="getJobApplicationDetails"),
 
-    path('nanny/hire-applications/', getNannyHireApplication, name="getNannyHireApplication"),
+    path('hire-applications/', getNannyHireApplication, name="getNannyHireApplication"),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
