1060ede34446b17b734c1fbbfdfdc0460c3d025a
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-25 13:10:06 +0200

    Setup smtp

--- kna/settings.py
+++ kna/settings.py
@@ -175,3 +175,12 @@ JAZZMIN_SETTINGS = {
     "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
     "related_modal_active": False,
 }
+
+# Email Backend Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = True  # Set to False if not using TLS
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Your App <dynasoftserver1@gmail.com>')
