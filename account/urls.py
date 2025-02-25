6e39596d4fb420c8625e9ebd1c2cab26d97ca927
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-25 13:31:35 +0200

    New hcnages

--- account/urls.py
+++ account/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('login/', userLogin, name="login"),
     path('logout/', userLogout, name='logout'),
     path('register/', userRegister, name="register"),
-    path('forget-password/', forgetPassword, name="forgetPassword"),
+
+    path('forget-password/', password_reset_request, name="forgetPassword"),
+    path('forget-password/confirm/', password_reset_confirm, name="forgetPasswordConfirm"),
 
     path('profile/', userProfile, name="profile"),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
