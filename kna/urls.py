6aab9652e09ffc3581995c4a5faacf710a9a676c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-12 13:50:10 +0200

    Add i18n URL configuration to support language switching in urls.py

--- kna/urls.py
+++ kna/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('account.urls')),
     path('', include('base.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
