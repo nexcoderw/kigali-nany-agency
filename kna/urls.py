060d85f88e8aacbd665a811836fc170eeeb0b4da
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-12 14:15:44 +0200

    New changes

--- kna/urls.py
+++ kna/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('account.urls')),
     path('', include('base.urls')),
-    path('i18n/', include('django.conf.urls.i18n')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
